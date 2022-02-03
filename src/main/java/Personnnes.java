32ed1319e3bc34c700ed6b83b6ac2555e729c59d
nathanieldouo <nathanieldouo@gmail.com>
2022-02-03 14:20:20 +0100

    fonction permettent a une personne de porter une montre

--- src/main/java/Personnnes.java
+++ src/main/java/Personnnes.java
@@ -1,2 +1,17 @@
-public class Personnnes {
+public class Personnnes<String> {
+    private String nom;
+    private Montres tool = null;
+    Personnnes( String nom){
+        this.nom=nom;
+
+    }
+    public void porter(Montres m){
+        if(this.tool == null){
+            this.tool = m;
+        }
+    }
+
+    public String getNom() {
+        return nom;
+    }
 }
