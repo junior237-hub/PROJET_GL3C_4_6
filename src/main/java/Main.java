f5d97e5d9c7431bb15b640ea61ffd421090b855a
Nguimene <juniorfopa@65gmail.com>
2022-02-03 14:55:20 +0100

    implementation de la fonction main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+    public Main() {
+    }
 
     public static void main(String[] args) {
-}
+        Montres m = new Montres(4, 54);
+
+        personnes p = new personnes("paul");
+        p.porter(m);
+        System.out.println("je suis:" + p.getNom());
+        p.affiche().Heure();
+        p.affiche().Min();
+        m.avance();
+        p.affiche().Heure();
+        p.affiche().Min();
+        p.retirer();
+        Montres montre_max = new Montres(12, 30);
+        personnes max = new personnes("max");
+        max.porter(montre_max);
+        p.demander(max);
+    }
 }
