aa3b9c5ddb84e1168aaeb5fe974332fdd6a4665a
ketsebo <patriciafamen@gmail.com>
2022-02-03 14:43:26 +0100

    fonction personne

--- src/main/java/Personnnes.java
+++ src/main/java/Personnnes.java
@@ -1,2 +1,20 @@
 public class Personnnes {
+
+    public Montres affiche(){
+        return  this.tool;
+    }
+    public void retirer(){
+        if(this.tool!=null){
+            this.tool = null;
+        }
+
+    }
+    public void demander(personnes p){
+        if(p.tool!=null){
+            System.out.println(" Sur la montre de "+p.getNom()+" il est "+p.tool.getHeures()+" : "+p.tool.getMin());
+        }
+        System.out.println("");
+    }
+}
+
 }
